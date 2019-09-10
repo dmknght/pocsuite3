75daa562bd937f6f0e7cb7e489b420afb74c9499
dmknght <dmknghtx2team@gmail.com>
2019-09-10 09:13:02 +0700

    Fix Update options

--- pocsuite3/lib/core/option.py
+++ pocsuite3/lib/core/option.py
@@ -640,6 +640,7 @@ def init():
 	_basic_option_validation()
 	_create_directory()
 	_init_kb_comparison()
+	update()
 	_set_multiple_targets()
 	_set_user_pocs_path()
 	_set_pocs_modules()
@@ -662,4 +663,3 @@ def init():
 	_set_threads()
 	_set_listener()
 	remove_extra_log_message()
-	update()
